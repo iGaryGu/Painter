c69d0321e1dc9d13cc59a14e6ff94af0ca12ae91
iGaryGu <gary640201a@gmail.com>
2015-01-07 20:36:31 +0800

    add dcmi and dma

--- src/main.c
+++ src/main.c
@@ -130,179 +130,9 @@ void camera_init(void)
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_DCMI);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_DCMI);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_DCMI);
-	/* i2c interface init */
-	GPIO_InitTypeDef i2c_gpio_init;
-
-	i2c_gpio_init.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-	i2c_gpio_init.GPIO_Mode = GPIO_Mode_AF;
-	i2c_gpio_init.GPIO_OType = GPIO_OType_OD;
-	i2c_gpio_init.GPIO_PuPd = GPIO_PuPd_UP;
-	i2c_gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init (GPIOB, &i2c_gpio_init);
-
-	GPIO_PinAFConfig (GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);    // I2C2_SCL
-	GPIO_PinAFConfig (GPIOB, GPIO_PinSource11, GPIO_AF_I2C2);    // I2C2_SDA
-	/* gpio init to provide mco frequency */
-	GPIO_InitTypeDef mco_Init;
-
-	mco_Init.GPIO_Pin = GPIO_Pin_8;
-	mco_Init.GPIO_Mode = GPIO_Mode_AF;
-	mco_Init.GPIO_Speed = GPIO_Speed_100MHz;
-	mco_Init.GPIO_OType = GPIO_OType_PP;
-
-	GPIO_Init(GPIOA, &mco_Init);
-
-	/*****************************************************************/
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM9, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-	/* PB4 = TIM3_CH1
-	 * PB5 = TIM3_CH2 */
-	GPIO_InitTypeDef tim3_gpio_init;
-	//GPIO_StructInit(&tim_gpio_init);
-	tim3_gpio_init.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_4;
-	tim3_gpio_init.GPIO_OType = GPIO_OType_PP;
-	tim3_gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-	tim3_gpio_init.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_Init(GPIOB, &tim3_gpio_init);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_TIM3);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_TIM3);
-
-	/* PA2 = TIM9_CH1
-	 * PA3 = TIM9_CH2 */
-	GPIO_InitTypeDef tim9_gpio_init;
-	//GPIO_StructInit(&tim_gpio_init);
-	tim9_gpio_init.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-	tim9_gpio_init.GPIO_OType = GPIO_OType_PP;
-	tim9_gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-	tim9_gpio_init.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_Init(GPIOA, &tim9_gpio_init);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_TIM9);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_TIM9);
-
-	//================================ TIM3 triggered input =============
-	/* configure TIM3 master mode to use OC1REF signal as trigger output */
-	TIM3->CR2 |= (TIM_CR2_MMS_2);
-	TIM3->CR2 &= ~(TIM_CR2_MMS_1 | TIM_CR2_MMS_0);
-	/* configure OC1REF to active level on compare match */
-	TIM3->CCMR1 |= (TIM_CCMR1_OC1M_0);
-	TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
-	/* configure compare register 1 to match on 20*tline  */
-	TIM3->CCR1 |= 20;
-	/* set master tim prescaler to match 1*tline = 1568, need to sub 1 for PSC */
-	TIM3->PSC |= (1567 * 2);
-	/* configure autoreload value and enable it */
-	TIM3->ARR &= 0;
-	TIM3->ARR |= 508;
-	TIM3->CR1 |= TIM_CR1_ARPE;
-	/* configure master to reset on rising edge of input trigger SMS == 100 */
-	TIM3->SMCR |= TIM_SMCR_SMS_2;
-	TIM3->SMCR &= ~(TIM_SMCR_SMS_1 | TIM_SMCR_SMS_0);
-	/* configure master to use TI2FP2 as input trigger TS == 110 */
-	TIM3->SMCR |= (TIM_SMCR_TS_2 | TIM_SMCR_TS_1);
-	TIM3->SMCR &= ~(TIM_SMCR_TS_0);
-
-	/* enable delay on trigger input for sync between master and slave */
-	TIM3->SMCR |= TIM_SMCR_MSM;
-
-	/* enable output to  on compare on channel 1 */
-	TIM3->CCER |= TIM_CCER_CC1E;
-
-	/* test output polarity low */
-	TIM3->CCER &= ~(TIM_CCER_CC1P);
-
-	/* enable interrupt on compare channel 1 */
-	TIM3->DIER |= TIM_DIER_CC1IE;
-	//============================== END PWM MODE TEST ==================
-
-	//================================ TIM9 triggered input =============
-	/* configure input reset source 1 - ITR1, TS == 001 */
-	TIM9->SMCR |= TIM_SMCR_TS_0;
-	TIM9->SMCR &= ~(TIM_SMCR_TS_1 | TIM_SMCR_TS_2);
-
-	/* configure TIM9 in reset mode SMS == 100 */
-	TIM9->SMCR &= ~(TIM_SMCR_SMS_1 | TIM_SMCR_SMS_0);
-	TIM9->SMCR |= (TIM_SMCR_SMS_2);
-
-	//	/* configure TIM9 in gated mode SMS == 101 */
-	//	TIM9->SMCR &= ~TIM_SMCR_SMS_1;
-	//	TIM9->SMCR |= (TIM_SMCR_SMS_2 | TIM_SMCR_SMS_0);
-
-	//	/* configure TIM9 in trigger mode SMS == 101 */
-	//	TIM9->SMCR &= ~TIM_SMCR_SMS_0;
-	//	TIM9->SMCR |= (TIM_SMCR_SMS_2 | TIM_SMCR_SMS_1);
-
-
-	//================================ PWM mode 1 on output ==============
-	TIM9->PSC |= 1;
-
-	TIM9->ARR &= 0;
-	TIM9->ARR |= 1567;
-	/* configure timer to enable auto-reload
-	 * and choose direction downcount */
-	TIM9->CR1 |= TIM_CR1_ARPE;
-	/* provide capture value for channel 1 */
-	TIM9->CCR1 |= 1279;
-
-	TIM9->CCMR1 &= ~(TIM_CCMR1_OC1M_0);
-	TIM9->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
-
-
-	//	/* set force inactive mode on initialization */
-	//	TIM9->CCMR1 &= ~(TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0);
-	//	TIM9->CCMR1 |= TIM_CCMR1_OC1M_2;
-
-
-	/* enable fast output change on compare - just in pwm mode 1 or 2*/
-	TIM9->CCMR1 |= TIM_CCMR1_OC1FE;
-	/* enable preload register */
-	TIM9->CCMR1 |= TIM_CCMR1_OC1PE;
-
-	/* select activity high polarity */
-	TIM9->CCER &= ~(TIM_CCER_CC1P);
-
-	/* enable the output */
-	TIM9->CCER |= TIM_CCER_CC1E;
-	/* enable interrupt event on compare channel 1 */
-	TIM9->EGR |= TIM_EGR_CC1G | TIM_EGR_UG;
-	//=============================== END PWM MODE TEST ==================
-	/* enable TIM9 to count */
-	//TIM9->CR1 |= TIM_CR1_CEN;
-
-	// ================================= COMPARE MODE ==================
-	//	TIM9->ARR &= 0;
-	//	TIM9->ARR |= 1280;
-	//	/* configure timer to enable auto-reload
-	//	 * and choose direction downcount */
-	//	TIM9->CR1 |= TIM_CR1_ARPE | TIM_CR1_DIR;
-	//	/* provide capture value for channel 1 */
-	//	TIM9->CCR1 |= 1;
-	//	/* choose OC1 to toggling on compare */
-	//	TIM9->CCMR1 &= ~(TIM_CCMR1_OC1M_2);
-	//	TIM9->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0);
-	//	/* disable preload register */
-	//	TIM9->CCMR1 &= ~(TIM_CCMR1_OC1PE);
-	//	/* select activity high polarity */
-	//	TIM9->CCER &= ~(TIM_CCER_CC1P);
-	//	/* enable the output */
-	//	TIM9->CCER |= TIM_CCER_CC1E;
-	//	/* enable interrupt event on compare channel 1 */
-	//	TIM9->DIER |= TIM_DIER_CC1IE;
-	// =========================================================
-/*	NVIC_InitTypeDef tim3_nvic_init;
-	tim3_nvic_init.NVIC_IRQChannel = TIM3_IRQn;
-	tim3_nvic_init.NVIC_IRQChannelCmd = DISABLE;
-	tim3_nvic_init.NVIC_IRQChannelPreemptionPriority = 1;
-	tim3_nvic_init.NVIC_IRQChannelSubPriority = 1;
-	NVIC_Init(&tim3_nvic_init);
-*/
-	/*****************************************************************/
-
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_MCO);
 
 	/* dcmi interface init */
-/*	DCMI_InitTypeDef camera_init;
+	DCMI_InitTypeDef camera_init;
 
 	camera_init.DCMI_CaptureMode = DCMI_CaptureMode_SnapShot;
 	camera_init.DCMI_CaptureRate = DCMI_CaptureRate_All_Frame;
@@ -314,82 +144,7 @@ void camera_init(void)
 
 	DCMI_StructInit(&camera_init);
 	// enable DCMI interface
-	DCMI_Cmd(ENABLE);
 
-	NVIC_InitTypeDef camera_irq;
-	camera_irq.NVIC_IRQChannel = DCMI_IRQn;
-	camera_irq.NVIC_IRQChannelPreemptionPriority = 0;
-	camera_irq.NVIC_IRQChannelSubPriority = 1;
-	camera_irq.NVIC_IRQChannelCmd = ENABLE;
-
-	NVIC_Init(&camera_irq);
-
-	DCMI_ITConfig(DCMI_IT_ERR | DCMI_IT_FRAME | DCMI_IT_LINE |
-			DCMI_IT_OVF | DCMI_IT_VSYNC, ENABLE);
-*/
-	I2C_InitTypeDef i2c_init;
-
-	i2c_init.I2C_Ack = I2C_Ack_Enable;
-	i2c_init.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-	i2c_init.I2C_ClockSpeed = 100000;
-	i2c_init.I2C_DutyCycle = I2C_DutyCycle_2;
-	i2c_init.I2C_Mode = I2C_Mode_I2C;
-	i2c_init.I2C_OwnAddress1 = 21;
-
-	I2C_Init(I2C2, &i2c_init);
-	// enable I2C interface
-	I2C_Cmd(I2C2, ENABLE);
-}
-
-void TIM3_IRQHandler(void)
-{
-	static volatile uint32_t flag = 0;
-
-	if(TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET)
-	{
-		if(flag == 0)
-		{
-			TIM3->CCR1 &= 0;
-			TIM3->CCR1 |= 499;
-
-			/* set inactive level on TRGO on compare match OC1M == 010*/
-			TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_2);
-			TIM3->CCMR1 |= TIM_CCMR1_OC1M_1;
-
-			//			TIM9->CCER |= TIM_CCER_CC1E;
-
-			//			/* choose OC1 to work in PWM generation mode 1 */
-			//			TIM9->CCMR1 &= ~(TIM_CCMR1_OC1M_0);
-			//			TIM9->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
-			//
-			TIM9->CR1 |= TIM_CR1_CEN;
-
-			flag = 1;
-		}
-		else
-		{
-			TIM3->CCR1 &= 0;
-			TIM3->CCR1 |= 20;
-			/* set active level on TRGO on compare match OC1M == 010*/
-			TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
-			TIM3->CCMR1 |= TIM_CCMR1_OC1M_0;
-
-			//			TIM9->CCER &= ~TIM_CCER_CC1E;
-
-			TIM9->CR1 &= ~TIM_CR1_CEN;
-
-			//			// DEBUG force inactive
-			//			TIM9->CCMR1 &= ~(TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0);
-			//			TIM9->CCMR1 |= TIM_CCMR1_OC1M_2;
-
-			flag = 0;
-		}
-		TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
-	}
-}
-
-void dma_init(void)
-{
 	DMA_DeInit(DMA2_Stream1);
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
@@ -407,7 +162,7 @@ void dma_init(void)
 	dma_init.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
 	dma_init.DMA_Mode = DMA_Mode_Normal;
 	dma_init.DMA_Priority = DMA_Priority_High;
-	dma_init.DMA_FIFOMode = DMA_FIFOMode_Enable;
+	dma_init.DMA_FIFOMode = DMA_FIFOMode_Disable;
 	dma_init.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
 	dma_init.DMA_MemoryBurst = DMA_MemoryBurst_Single;
 	dma_init.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
@@ -425,17 +180,66 @@ void dma_init(void)
 
 	NVIC_Init(&dma_stream1_irq);
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-}
 
-void DMA2_Stream1_IRQHandler(void)
+	NVIC_InitTypeDef camera_irq;
+	camera_irq.NVIC_IRQChannel = DCMI_IRQn;
+	camera_irq.NVIC_IRQChannelPreemptionPriority = 0;
+	camera_irq.NVIC_IRQChannelSubPriority = 1;
+	camera_irq.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&camera_irq);
+
+	DCMI_ITConfig(DCMI_IT_ERR | DCMI_IT_FRAME | DCMI_IT_LINE |
+			DCMI_IT_OVF | DCMI_IT_VSYNC, ENABLE);
+
+	/* i2c interface init */
+	GPIO_InitTypeDef i2c_gpio_init;
+
+	i2c_gpio_init.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+	i2c_gpio_init.GPIO_Mode = GPIO_Mode_AF;
+	i2c_gpio_init.GPIO_OType = GPIO_OType_OD;
+	i2c_gpio_init.GPIO_PuPd = GPIO_PuPd_UP;
+	i2c_gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init (GPIOB, &i2c_gpio_init);
+
+	GPIO_PinAFConfig (GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);    // I2C2_SCL
+	GPIO_PinAFConfig (GPIOB, GPIO_PinSource11, GPIO_AF_I2C2);    // I2C2_SDA
+	I2C_InitTypeDef i2c_init;
+
+	i2c_init.I2C_Ack = I2C_Ack_Enable;
+	i2c_init.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	i2c_init.I2C_ClockSpeed = 100000;
+	i2c_init.I2C_DutyCycle = I2C_DutyCycle_2;
+	i2c_init.I2C_Mode = I2C_Mode_I2C;
+	i2c_init.I2C_OwnAddress1 = 21;
+
+	I2C_Init(I2C2, &i2c_init);
+	// enable I2C interface
+	I2C_Cmd(I2C2, ENABLE);
+/*********************************************************/
+
+	/* gpio init to provide mco frequency */
+	GPIO_InitTypeDef mco_Init;
+
+	mco_Init.GPIO_Pin = GPIO_Pin_8;
+	mco_Init.GPIO_Mode = GPIO_Mode_AF;
+	mco_Init.GPIO_Speed = GPIO_Speed_100MHz;
+	mco_Init.GPIO_OType = GPIO_OType_PP;
+
+	GPIO_Init(GPIOA, &mco_Init);
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_MCO);
+
+}
+void SendPicture(void)
 {
-	if(DMA_GetITStatus(DMA2_Stream1, DMA_IT_TCIF1) != RESET)
+	int i;
+	for(i=0;i<10;i++)
 	{
-		dma_handler_counter++;
-		DMA_ClearITPendingBit(DMA2_Stream1, DMA_IT_TCIF1);
+		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+		USART_SendData(USART1, camera_frame[i]);
 	}
 }
-
 
 char* itoa(int value, char* result, int base)
 {
@@ -461,6 +265,66 @@ char* itoa(int value, char* result, int base)
 	}
 	return result;
 }
+int count = 0;
+char strr[10] = "";
+int flag = 0;
+void DCMI_IRQHandler(void)
+{
+	if(DCMI_GetFlagStatus(DCMI_FLAG_VSYNCRI) == SET)
+	{
+		if(flag!=1){
+			USART1_puts("dcmi VSYNCR1\r\n");
+		}
+		DCMI_ClearFlag(DCMI_FLAG_VSYNCRI);
+	}
+	else if(DCMI_GetFlagStatus(DCMI_FLAG_LINERI) == SET)
+	{
+		if(flag!=1){
+			USART1_puts("dcmi LINERI\r\n");
+		}
+		DCMI_ClearFlag(DCMI_FLAG_LINERI);
+	}
+	else if(DCMI_GetFlagStatus(DCMI_FLAG_FRAMERI) == SET)                                                   
+	{
+		flag = 1;
+//		USART1_puts("DCMI_FLAG_FRAMERI \n\n");
+		DCMI_ClearFlag(DCMI_FLAG_FRAMERI);
+	}
+	else if(DCMI_GetFlagStatus(DCMI_FLAG_ERRRI) == SET)
+	{
+		USART1_puts("DCMI_FLAG_ERRRI \n\n");
+		DCMI_ClearFlag(DCMI_FLAG_ERRRI);
+	}
+	else if(DCMI_GetFlagStatus(DCMI_FLAG_OVFRI) == SET)
+	{
+		flag = 1;
+		USART1_puts("DCMI_FLAG_OVFRI \n\n");
+		DCMI_ClearFlag(DCMI_FLAG_OVFRI);
+	}
+}
+
+
+char st[10]="";
+void DMA2_Stream1_IRQHandler(void)
+{
+	if(DMA_GetITStatus(DMA2_Stream1, DMA_IT_TCIF1) != RESET)
+	{
+		int r = DCMI_ReadData();
+		itoa(r,st,10);
+		USART1_puts(st);
+		USART1_puts("\r\n");
+		USART1_puts("DMA handle!\r\n");
+		
+		dma_handler_counter++;
+		DMA_ClearITPendingBit(DMA2_Stream1, DMA_IT_TCIF1);
+	}
+	if(DMA_GetITStatus(DMA2_Stream1,DMA_IT_TEIF1) == SET){
+		USART1_puts("DMA2 error\r\n");
+		DMA_ClearITPendingBit(DMA2_Stream1, DMA_IT_TEIF1);
+	}
+}
+
+
 int main(void)
 {
 	uint8_t c = 0;
@@ -481,15 +345,7 @@ int main(void)
 
 	GPIO_Init(GPIOG, &GPIOG_Init);
 	camera_init();
-	//    Delay(1000);
-	//i2c_init();
-	//camera_init();
-	//dma_init();
 	c = I2C_readreg(0x0a);
-//	NVIC_EnableIRQ(TIM3_IRQn);
-
-	//    TIM9->CR1 |= TIM_CR1_CEN;
-	TIM3->CR1 |= TIM_CR1_CEN;
 
 	if(c == 0x76)
 	{
@@ -497,12 +353,16 @@ int main(void)
 		GPIO_SetBits(GPIOG, GPIO_Pin_13);
 	}
 
-//	    DMA_Cmd(DMA2_Stream2, ENABLE);
-
+	DMA_Cmd(DMA2_Stream1, ENABLE);
+	DCMI_Cmd(ENABLE);
+	DCMI_CaptureCmd(ENABLE);
+	
 	while(1)
 	{
-		if(dma_handler_counter == 10)
+		if(dma_handler_counter == 10){
+			SendPicture();
 			GPIO_SetBits(GPIOG, GPIO_Pin_14);
+		}
 	}
 
 	return 0;
